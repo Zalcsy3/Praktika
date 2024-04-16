0233a192b0af8469b62597cca76655ec09ce0660
Becze Balazs <balazs7699@gmail.com>
2024-04-16 17:12:35 +0300

    feat: add project stucture and todoes

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+//TODO: itt kell behivni a themat ahogy a MUI-ba megnezem hogy kell, egy Provider lesz
+//TODO: itt kell osszerakni a route-kat es akkor o tudja melyik linken melyik paget hivja be
+//TODO: behuzok egy peldat az appra
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

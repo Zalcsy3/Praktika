705686e5ee429dfe79f2e4bde8ecad4372800c79
Becze Balazs <balazs7699@gmail.com>
2024-04-18 11:45:01 +0300

    feat: implement login

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import Layout from '../../components/layout/Layout'
+import React from "react";
+import Layout from "../../components/layout/Layout";
+import {
+  Box,
+  Button,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 const LoginPage = () => {
   return (
-    <Layout>LoginPage</Layout>
-  )
-}
+    <Layout>
+      <Stack width={"100%"} alignItems={"center"}>
+        <Paper
+          sx={{
+            width: 500,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            p: 4,
+            borderRadius: 4
+          }}
+        >
+          <Stack alignItems={"center"} justifyContent={"center"} gap={2}>
+            <Typography variant="h4" sx={{ mb: 3 }}>
+              Login
+            </Typography>
+            <TextField placeholder="Username" label="Username" />
+            <TextField placeholder="Username" label="Username" />
+            <Button sx={{ mt: 3 }} variant="contained">
+              Login
+            </Button>
+          </Stack>
+        </Paper>
+      </Stack>
+    </Layout>
+  );
+};
 
-export default LoginPage
+export default LoginPage;

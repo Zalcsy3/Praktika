57bd4d71a33f3c98a586389b91245b47a60f33b3
Becze Balazs <balazs7699@gmail.com>
2024-04-25 13:11:31 +0300

    refactor: add todoes

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,6 +3,7 @@ import { createTheme } from '@mui/material/styles';
 export const theme = createTheme({
   palette: {
     primary: {
+      //TODO: ertelmetlen a mainnek a feheret adni, valami olyant adj amit sokat hasznalsz, mert a feheret tudud ugy is hogy 'white' hasznalni
       main: '#fff', //feher
       
        
@@ -14,6 +15,7 @@ export const theme = createTheme({
 
     },
     text: {
+      //TODO: ennek is ugyan az a feketet hasznalhatod hogy 'black' ide custom szinek kellenek ha vannak
       primary: '#000', //fekete
        //szurke
     },
@@ -29,6 +31,7 @@ export const theme = createTheme({
       styleOverrides: {
         root: {
           borderRadius: '15px',
+          //TODO: pl egy ilyen szint kivenni es hasznalni hogy ne hex kodot adj itt
           backgroundColor:'#045bb1', //kek
           color:'#fff',
         },
